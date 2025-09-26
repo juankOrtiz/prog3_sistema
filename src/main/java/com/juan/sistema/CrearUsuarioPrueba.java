518ea6db087dbf25e1d4457989b069c0ade325f2
juankOrtiz <juankortiz@proton.me>
2025-09-25 21:09:42 -0300

    Crear usuario de prueba en la BD

---
+++ src/main/java/com/juan/sistema/CrearUsuarioPrueba.java
@@ -0,0 +1,26 @@
+package com.juan.sistema;
+
+import com.juan.sistema.util.DatabaseConnection;
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class CrearUsuarioPrueba {
+    public static void main(String[] args) {
+        String nombre = "admin";
+        String password = "admin";
+        String password_encriptada = BCrypt.hashpw(password, BCrypt.gensalt());
+        String consulta = "INSERT INTO usuarios(nombre, password) VALUES (?, ?)";
+        try (Connection conn = DatabaseConnection.getConexion()) {
+            PreparedStatement stmt = conn.prepareStatement(consulta);
+            stmt.setString(1, nombre);
+            stmt.setString(2, password_encriptada);
+            int filas = stmt.executeUpdate();
+            System.out.println(filas > 0);
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+}
